933e10a9e9c9402522afe86076332efac4e93f3f
nwilkens4 <131278440+nwilkens4@users.noreply.github.com>
2023-04-25 15:49:38 -0400

    edited html to take in hours and minutes.

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,16 @@
 let timerId;
 
 function startTimer() {
-  const timeLimit = parseInt(document.getElementById('time-limit').value);
-  const timeInSeconds = timeLimit * 60;
+  const hours = parseInt(document.getElementById('hours').value);
+  const minutes = parseInt(document.getElementById('minutes').value);
+  const timeInSeconds = (hours * 60 * 60) + (minutes * 60);
 
   timerId = setTimeout(() => {
     // Close the Amazon tab
     chrome.tabs.query({ url: 'https://www.amazon.com/*' }, (tabs) => {
       if (tabs && tabs.length > 0) {
         chrome.tabs.remove(tabs[0].id);
-        alert(`Time's up! You've reached your time limit of ${timeLimit} minutes.`);
+        alert(`Time's up! You've reached your time limit of ${hours} hours and ${minutes} minutes.`);
       }
     });
   }, timeInSeconds * 1000);
