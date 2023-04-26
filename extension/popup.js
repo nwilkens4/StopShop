c2f9250a228503fc14917d5fc82f689aef91053c
nwilkens4 <131278440+nwilkens4@users.noreply.github.com>
2023-04-26 09:49:24 -0400

    first commit of the day 4/26

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,14 @@
 let timerId;
 
+function showTimeLimitContainer() {
+  const setTimeLimitButton = document.getElementById('set-time-limit');
+  const setTimeLimitContainer = document.querySelector('.set-time-limit-container');
+
+  setTimeLimitButton.addEventListener('click', () => {
+    setTimeLimitButton.style.display = 'none';
+    setTimeLimitContainer.style.display = 'block';
+  });
+}
 function startTimer() {
   const hours = parseInt(document.getElementById('hours').value);
   const minutes = parseInt(document.getElementById('minutes').value);
@@ -10,15 +19,19 @@ function startTimer() {
     chrome.tabs.query({ url: 'https://www.amazon.com/*' }, (tabs) => {
       if (tabs && tabs.length > 0) {
         chrome.tabs.remove(tabs[0].id);
-        alert(`Time's up! You've reached your time limit of ${hours} hours and ${minutes} minutes.`);
+        alert(`Stop Shopping! You've reached your time limit of ${hours} hours and ${minutes} minutes.`);
       }
     });
   }, timeInSeconds * 1000);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  showTimeLimitContainer();
 
 document.getElementById('start-timer').addEventListener('click', () => {
   if (timerId) {
     clearTimeout(timerId);
   }
   startTimer();
+});
 });
